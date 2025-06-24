056b988c5a31bcf9b2ca15508149c10b24d3477c
Mathieu <mathieu762005@gmail.com>
2025-06-24 09:28:59 +0200

    mise a jour

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-function add(a, b) {
-    return a + b
-}
-function subtract(a, b) {
-    return a - b
-}
-function multiply(a, b) {
-    return a * b
-}
-function divide(a, b) {
-    return a / b
+let affiche = document.getElementById(affiche)
+
+
+function add(num1, num2) {
+    return num1 + num2
 }
 
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+
+function addNumber(nb) {
+    document.getElementById(affiche).innerText += nb
+}
+
+function addOpperande(opperande) {
+    affiche = "num2"
+    document.getElementById(opperande)
+}
+
+
 function calculer(operation, num1, num2) {
-    if(operation == add()){
+    if (operation == add()) {
         return add(num1, num2)
-    } else if(operation == subtract()){
+    } else if (operation == subtract()) {
         return subtract(num1, num2)
-    } else if(operation == multiply()){
+    } else if (operation == multiply()) {
         return multiply(num1, num2)
-    } else if(operation == divide()){
+    } else if (operation == divide()) {
         return divide(num1, num2)
     }
     return operation
